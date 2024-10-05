5d6d8d1745443745bc1ad3cfc1dc870a42d38d2a
michall <michall@pzh.gov.pl>
2024-10-05 21:48:23 +0200

    Skrypt sequencing_summary_json_generator.py generuje jsona zgodnie z dokumentacja dla outputu fastqc, nalezy wpiac go w pipeline

---
+++ bin/sequencing_summary_json_generator.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python3
+"""
+Skrypt do parsowania wynikow programu fastqc i produkowania json-a zgodnego ze specyfikacja w repozytorium plepiseq_json
+"""
+
+import sys
+import numpy as np
+import click
+import json
+
+
+@click.command()
+@click.option('-i', '--input_file', help='[INPUT] a path to a summary file produced with fastqc ',
+              type=click.Path(), default="fastqc_data.txt")
+@click.option('-f', '--filename', help='[INPUT] name of a file used for fastqc analysis, required if '
+                                       'status is different than tak',
+              type=str, default="")
+@click.option('-s', '--status', help='[INPUT] Status that is transferred to an output json ',
+              type=click.Choice(['tak', 'nie', 'blad'], case_sensitive=False),  required=True)
+@click.option('-e', '--stage', help='[INPUT] Stage on which data is analyzed',
+              type=click.Choice(["pre-filtering", "post-filtering"], case_sensitive=False),  required=True)
+@click.option('-p', '--publishdir', help='[INPUT] Path with fNEXTFLOW output, required to correctly format json',
+              type=str,  required=True)
+@click.option('-m', '--error', help='[INPUT] Error that is put in json',
+              type=str,  required=False, default="")
+@click.option('-o', '--output', help='[Output] Name of a file with json output',
+              type=str,  required=False)
+def main_program(input_file, status, stage, publishdir, output, error, filename):
+    if status == "nie" or status == 'blad':
+        json_dict = [{"status": status, "file_name": filename, "step_name": stage, "error_message": error}]
+        with open(output, 'w') as f1:
+            f1.write(json.dumps(json_dict))
+        return True
+    else:
+        try:
+            open(input_file)
+        except FileNotFoundError:
+            json_dict = [{"status": "error", "file_name": filename, "step_name": stage,
+                         "error_message": "Provided file does not exists"}]
+            with open(output, 'w') as f1:
+                f1.write(json.dumps(json_dict))
+            return True
+
+        number_of_reads_value = 0  # pole number_of_reads_value
+        number_of_bases_value = 0  # pole number_of_bases_value
+        reads_median_length_value = 0  # pole reads_median_length_value
+        reads_min_length_value = 0  # reads_min_length_value
+        reads_max_length_value = 0  # pole reads_max_length_value
+        reads_median_quality_value = 0  # pole reads_median_quality_value
+        reads_quality_histogram_path = f'{stage}_reads_quality_histogram.csv'  # pole reads_quality_histogram_path
+        reads_length_histogram_path = f'{stage}_reads_length_histogram.csv'  # pole reads_length_histogram_path
+        position_quality_plot_path = f'{stage}_position_quality_plot.csv'
+        gc_content_value = 0  # OPCJONALNE pole gc_content_value
+
+        reads_median_quality_data = []  # pomocnicza lista do obliczenia mediany
+        reads_median_length_data = []  # pomocnicza lista do obliczenia mediany
+        start = 0
+        section_name = ""
+        # otwieramy polaczenia do plikow z wymaganymi nazwami
+        # tworzymy tez naglowki zgodnie z dokumentacja excelu
+        qualty_histogram_file = open(reads_quality_histogram_path, 'w')
+        qualty_histogram_file.write(f'#indeks;jakosc_wartosc;liczebnosc\n')
+
+        length_histogram_file = open(reads_length_histogram_path, 'w')
+        length_histogram_file.write(f'#indeks;dlugosc_wartosc;liczebnosc\n')
+
+        position_quality_file = open(position_quality_plot_path, "w")
+        position_quality_file.write(f'#indeks;pozycja_w_odczycie;mediana_jakosc\n')
+        with open(input_file) as f:
+            for line in f:
+                line = line.split('\t')
+                if ">>" in line[0] and start == 0:
+                    # Sekcje z rownymi typami wynikow w pliku rozpoczynaja sie od ">>"
+                    # a koncza na ">>END_MODULE"
+                    start = 1
+                    section_name = line[0].replace(">>", "")
+                elif ">>" in line[0] and start == 1:
+                    start = 0
+                    if section_name == "Per sequence quality scores":
+                        reads_median_quality_value = np.median(reads_median_quality_data)
+                    # koniec sekcji mozna zrzucac wyniki
+                    if section_name == "Sequence Length Distribution":
+                        reads_median_length_value = np.median(reads_median_length_data)
+
+                elif start == 1:
+                    # jestem s sekcji
+                    if section_name == "Basic Statistics":
+                        if line[0] == "Filename":
+                            nazwa_pliku = line[1]
+                        if line[0] == "Total Sequences":
+                            number_of_reads_value = line[1]
+                        if line[0] == "Sequence length":
+                            reads_min_length_value, reads_max_length_value = line[1].split("-")
+                        if line[0] == "Total Bases":
+                            number_of_bases_value = float(line[1].split(" ")[0])
+                            unit = line[1].split(" ")[1].rstrip()  # fastqc dodaje okreslecia jak Kbp, Mbp i pewnie Gbp
+                            if unit == "Kbp":
+                                number_of_bases_value = number_of_bases_value * 1000
+                            elif unit == "Mbp":
+                                number_of_bases_value = number_of_bases_value * 1000000
+                            elif unit == "Gbp":
+                                number_of_bases_value = number_of_bases_value * 1000000000
+                        if line[0] == "%GC":
+                            gc_content_value = line[1]
+
+                    if section_name == "Per sequence quality scores":
+                        if "#" in line[0]:
+                            indeks = 0
+                        else:
+                            qualty_histogram_file.write(f'{indeks};{line[0]};{line[1]}')
+                            reads_median_quality_data.extend([int(line[0])] * int(float(line[1].rstrip())))
+                            indeks += 1
+                    if section_name == "Sequence Length Distribution":
+                        if "#" in line[0]:
+                            indeks = 0
+                        else:
+                            # fastqc przekazuje dane w postaci od-do np 30-35,
+                            length_histogram_file.write(f'{indeks};{line[0].split("-")[0]};{line[1]}')
+                            reads_median_length_data.extend([int(line[0].split("-")[0])] * int(float(line[1].rstrip())))
+                            indeks += 1
+                    if section_name == "Per base sequence quality":
+                        if "#" in line[0]:
+                            indeks = 0
+                        else:
+                            if len(line[0].split("-")) == 2:
+                                pozycja = line[0].split("-")[1]
+                            elif len(line[0].split("-")) == 1:
+                                pozycja = line[0].split("-")[0]
+                            else:
+                                json_dict = [{"status": "error", "file_name": input_file, "step_name": stage,
+                                             "error_message": "Error when parsin fastqc file"}]
+                                with open(output, 'w') as f1:
+                                    f1.write(json.dumps(json_dict))
+                                return True
+
+                            position_quality_file.write(f"{indeks};{pozycja};{line[2]}\n")
+    json_dict = [{"status": "tak",
+                 "file_name": nazwa_pliku.rstrip(),
+                 "step_name": stage,
+                 "error_message": "",
+                 "number_of_reads_value": int(number_of_reads_value),
+                 "number_of_bases_value": int(number_of_bases_value),
+                 "reads_median_length_value": round(float(reads_median_length_value),2),
+                 "reads_min_length_value": int(reads_min_length_value),
+                 "reads_max_length_value": int(reads_max_length_value),
+                 "reads_median_quality_value": round(float(reads_median_quality_value),2),
+                 "reads_quality_histogram_path": f"{publishdir}/{reads_quality_histogram_path}",
+                 "reads_length_histogram_path": f"{publishdir}/{reads_length_histogram_path}",
+                 "position_quality_plot_path": f"{publishdir}/{position_quality_plot_path}",
+                 "gc_content_value": round(float(gc_content_value), 2)}]
+    with open(output, 'w') as f1:
+        f1.write(json.dumps(json_dict))
+    return True
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        main_program(['--help'])
+    else:
+        main_program(sys.argv[1:])
